c857371e0b4a1302838edd2ebf15c0104c2e4d84
Sa33D <saeidsorkh.76@gmail.com>
2025-02-01 23:08:16 +0330

    init

---
+++ src/test/java/saeed/sorkh/SimpleBank/SimpleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package saeed.sorkh.SimpleBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
