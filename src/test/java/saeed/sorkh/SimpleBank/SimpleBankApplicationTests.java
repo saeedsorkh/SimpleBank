b16a3162072b9cd5d6e9641e9122ad89ea5f575c
Sa33D <saeidsorkh.76@gmail.com>
2025-02-01 23:09:12 +0330

    Commented tests

--- src/test/java/saeed/sorkh/SimpleBank/SimpleBankApplicationTests.java
+++ src/test/java/saeed/sorkh/SimpleBank/SimpleBankApplicationTests.java
@@ -1,13 +1,11 @@
 package saeed.sorkh.SimpleBank;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SimpleBankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    //	@Test
+    void contextLoads() {
+    }
 
 }
