66a3f3dca51b1cf395275d880c620799d702b791
Sa33D <saeidsorkh.76@gmail.com>
2025-02-02 01:50:47 +0330

    Added a very very simple Exception handler, just to get custom exceptions messages :)

---
+++ src/main/java/saeed/sorkh/SimpleBank/config/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package saeed.sorkh.SimpleBank.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleExceptions(Exception ex, HttpServletRequest request) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("timestamp", LocalDateTime.now());
+        errorDetails.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorDetails.put("error", "Internal Server Error");
+        errorDetails.put("message", ex.getMessage());
+        errorDetails.put("path", request.getRequestURI());
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
