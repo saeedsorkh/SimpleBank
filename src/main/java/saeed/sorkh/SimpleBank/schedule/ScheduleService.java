5a9708614efb0e2ba1adb5b9b5d62e1dbdb86054
Sa33D <saeidsorkh.76@gmail.com>
2025-02-02 23:13:05 +0330

    Updated package naming

--- src/main/java/saeed/sorkh/SimpleBank/schedule/ScheduleService.java
+++ src/main/java/saeed/sorkh/SimpleBank/schedule/ScheduleService.java
@@ -1,4 +1,4 @@
-package saeed.sorkh.SimpleBank.scheduled;
+package saeed.sorkh.SimpleBank.schedule;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
